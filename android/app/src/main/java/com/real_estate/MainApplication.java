7fe84c047b88e7fd2d4990950998dd339d63d823
Dương Ngọc Tú <dntu622@gmail.com>
2018-03-27 14:59:03 +0700

    commit

--- android/app/src/main/java/com/real_estate/MainApplication.java
+++ android/app/src/main/java/com/real_estate/MainApplication.java
@@ -3,8 +3,6 @@ package com.real_estate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import co.apptailor.googlesignin.RNGoogleSigninPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,8 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGoogleSigninPackage(),
-            new FBSDKPackage(),
           new MapsPackage()
       );
     }
